3666dee45f948a7ae256b1def8cc27e901bb216d
Clare Bear <97364648+AstroClareTechnology@users.noreply.github.com>
2024-01-18 15:05:35 -0500

    COMMITT
    COMMITT THESE

--- script/main.js
+++ script/main.js
@@ -1,16 +1,10 @@
-<<<<<<< Updated upstream
-function menu(){
-    if (document.getElementById("floatNav").style.display == "none"){
-        document.getElementById("floatNav").style.display = "block";
-    }else{
-=======
+
 function mobMenu(){
     if(document.getElementById("floatNav").style.display == "none"){
         document.getElementById("floatNav").style.display = "block";
         document.getElementById("exit").style.display = "block";
     }
     else{
->>>>>>> Stashed changes
         document.getElementById("floatNav").style.display = "none";
     }
 }
